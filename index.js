bfc7276ad81f51b6291441157a7bf1e0274610cd
thisistony <tonynz@gmail.com>
2017-03-17 11:26:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var AWS = require('aws-sdk'),
+q = require ('q'),
+request = require('request'),
+kfapikey = require('./config/kfapikey.json'),
+promise = require('promise');
+
+function getSources(){
+  var deferred = q.defer();
+  var url = "https://app.klipfolio.com/api/1/datasources?limit=100";
+  var headers =
+  {
+    'kf-api-key'   : kfapikey.kfapikey
+  };
+  request.get(url, {headers: headers}, function (error, response, body) {
+    var array = JSON.parse(body);
+    var parsed = array.data
+    deferred.resolve(parsed);
+  });
+  return deferred.promise;
+}
+
+module.exports.run = function () {
+  getSources().then(function(parsed){
+    var length = parsed.datasources.length;
+    console.log("Total data sources: "+length);
+    var dataSourceList = [];
+    var a = 0;
+    for(var a=0; a<length; a++){
+      if(parsed.datasources[a].description=="Adobe Analytics"){
+        dataSourceList.push(parsed.datasources[a].id)
+      }
+    }
+    console.log("We currently have "+dataSourceList.length+" Adobe Analytics data sources. Performing API push.");
+    var incrementer = 0;
+    var resultsArray = [];
+    var b = -1
+    var myInterval = setInterval(function() {
+      incrementer++
+      b++
+      if(incrementer == dataSourceList.length){
+        clearInterval(myInterval)
+        console.log("Finished!")
+      }
+      var url = "https://app.klipfolio.com/api/1.0/datasource-instances/"+dataSourceList[b]+"/@/refresh";
+      var headers =
+      {
+        'kf-api-key'   : kfapikey.kfapikey
+      };
+      request.post(url, {headers: headers}, function (error, response, body) {
+        var array = JSON.parse(body);
+        console.log(b+ " - "+dataSourceList[b]+": "+array.meta.status);
+        //error handles disabled connections.
+        if(array.meta.status == 403){
+          console.log("Data Source disabled. Re-enabling datasource.");
+          var enabler = "https://app.klipfolio.com/api/1.0/datasources/"+dataSourceList[b]+"/@/enable";
+          request.post(enabler, {headers: headers}, function (error, response, body) {
+            var array = JSON.parse(body);
+            console.log(b+ " - "+dataSourceList[b]+": "+body);
+            b--
+          });
+          //
+        }
+      });
+    },1100);
+  });
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+## Why do I need this?
+
+Klipfolio currently only updates data sources if there is an user logged in. So if someone logs in for the first time in the morning, it will take a few minutes for fresh data  to be delivered.
+
+This script will update all Klipfolio data sources in the background regardless of the requirement.
+
+## How does it work?  
+
+It utilises the Klipfolio API firstly by downloading a list of all of data sourve IDs, and then loops through and force a refresh data sources with "Adobe Analytics" in the description.
+
+Please feel free to modify the script if you want it to force refresh every data source.
+
+## How do I install it?
+
+**Download the script** - modify the ./config/ by putting in your Klipfolio API key in. For more information, please go to: https://support.klipfolio.com/hc/en-us/articles/215548478-Generate-a-Klipfolio-API-Key
+
+**Create a new Lambda function** - Zip it all up containing your Klipfolio API ID and upload.
+
+**Schedule it using CloudWatch** - You can specify it to run at any interval you wish. Just use cron.
+
+## Where do I get more information?
+
+Feel free to contact me on hello@thisistony.com
